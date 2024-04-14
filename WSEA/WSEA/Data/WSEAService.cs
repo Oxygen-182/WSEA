c278d4533db8651f8d0acda6ae253d8e2a9fad86
Salieri <Salieri@DESKTOP-QL85CJN>
2024-04-14 14:50:21 +0300

    Добавлена страница добавления объекта для риэлтора

--- WSEA/WSEA/Data/WSEAService.cs
+++ WSEA/WSEA/Data/WSEAService.cs
@@ -11,6 +11,30 @@ namespace WSEA.Data
             _context = context;
         }
 
+        public Task<RealtyImage> CreateRealtyImage(RealtyImage realtyImage)
+        {
+            _context.RealtyImages.Add(realtyImage);
+            _context.SaveChanges();
+            return Task.FromResult(realtyImage);
+        } 
+
+        public Task<Realty> CreateRealtyAsync(Realty realty)
+        {
+            _context.Realties.Add(realty);
+            _context.SaveChanges();
+            return Task.FromResult(realty);
+        }
+
+        public async Task<Realty?> GetRealtyAsync(int id_realty)
+        {
+            return await _context.Realties
+                .AsNoTracking()
+                .Where(x => x.IdRealty == id_realty)
+                .Include(x => x.RealtyImages)
+                .Include(x => x.RealtyType)
+                .FirstOrDefaultAsync();
+        }
+
         public async Task<List<Realty>> GetRealtiesAsync()
         {
             return await _context.Realties
